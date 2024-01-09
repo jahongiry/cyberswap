baa7639ec2c010274deb75da0e25d6a780e6428b
azimjonking <azimjonjalilov1301@mail.ru>
2024-01-10 00:02:53 +0200

    home page media

--- src/component/favouriteGames/FavouriteGames.js
+++ src/component/favouriteGames/FavouriteGames.js
@@ -1,25 +1,136 @@
-import React from 'react';
-import './favouriteGames.css';
+import React from "react";
+import "./favouriteGames.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// import game1 from "./img/game1.png";
+import game2 from "./img/game2.png";
+// import game3 from "./img/game3.png";
+
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="control-btn" onClick={onClick}>
+      <i className="fa-solid fa-angle-left prev"></i>
+    </div>
+  );
+};
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="control-btn" onClick={onClick}>
+      <i className="fa-solid fa-angle-right next"></i>
+    </div>
+  );
+};
 
 const FavouriteGames = () => {
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+
+    dots: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    appenDots: (dots) => {
+      return <ul style={{ margin: "0px" }}>{dots}</ul>;
+    },
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
   return (
-    <section className='favouriteGames'>
-      <h1 className='title'>
+    <section className="favouriteGames">
+      <h1 className="title">
         O'zingizga mos <br />
         <span>takliflarni</span> toping
       </h1>
-      <p className='subtitle'>
+      <p className="subtitle">
         Eng arzon va yuqori darajadagi pubg accountlar <br />
         Ishonchli va qiziquvchan haridorlar, real timedagi savdo
       </p>
 
-      <div className='favouriteGames-container'></div>
+      <div className="favouriteGames-container">
+        <Slider {...settings}>
+          <div className="favouriteGames-content">
+            {/* <div className="favouriteGame-img">
+            <img src={game1} alt="Game1" />
+          </div> */}
+          </div>
+          <div className="favouriteGames-content">
+            {/* <div className="favouriteGame-img">
+            <img src={game1} alt="Game1" />
+          </div> */}
+          </div>
 
-      <div className='favouriteGames-btns'>
-        <button className='btn'>
+          <div className="favouriteGames-content">
+            <div className="favouriteGame-img">
+              <img src={game2} alt="Game1" />
+            </div>
+
+            {/* position frame start */}
+            <div className="left-side position-side position-frame">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="right-side position-side position-frame">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="top-center position-frame">
+              <i className="fa-solid fa-gamepad"></i>
+            </div>
+            <div className="top-left__side position-side position-frame">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="top-right__side position-side position-frame">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+
+            <div className="bottom-center position-frame">
+              <p>sneak peeks</p>
+            </div>
+            {/* position frame end */}
+          </div>
+
+          <div className="favouriteGames-content">
+            {/* <div className="favouriteGame-img">
+            <img src={game3} alt="Game1" />
+          </div> */}
+          </div>
+        </Slider>
+      </div>
+
+      <div className="favouriteGames-btns">
+        <button className="btn">
           <span>Accountarni ko'rish</span>
         </button>
-        <button className='btn'>
+        <button className="btn">
           <span>Yangi e'lon yaratish</span>
         </button>
       </div>
--- src/component/mainSection/MainSection.js
+++ src/component/mainSection/MainSection.js
@@ -1,44 +1,67 @@
-import React from 'react';
-import './mainSection.css';
+import React from "react";
+import "./mainSection.css";
+import homeImg from "./logo1_1.png";
 
 const MainSection = () => {
   return (
-    <section className='mainSection container'>
-      <div className='home-container'>
-        <h1 className='title'>
-          biz bilan <br />
-          <span>"Kiber savdo"</span>
-          <br />
-          Garant va Ishonchli
-        </h1>
-        <p className='subtitle'>
-          Oyin accountlari, Telegram nomlar va kanallar, Instagram nomlarini
-          havsiz soting va sotib oling!
-        </p>
+    <section className="mainSection container">
+      <div className="home-container">
+        <div className="home-container__info">
+          <h1 className="title">
+            biz bilan <br />
+            <span>"Kiber savdo"</span>
+            <br />
+            Garant va Ishonchli
+          </h1>
+          <p className="subtitle">
+            Oyin accountlari, Telegram nomlar va kanallar, Instagram nomlarini
+            havsiz soting va sotib oling!
+          </p>
 
-        <div className='home-btns'>
-          <button className='btn'>
-            <span>Elon berish/Sotish</span>
-          </button>
-          <button className='btn'>
-            <span>Sotib olish</span>
-          </button>
+          <div className="home-btns">
+            <button className="btn">
+              <span>Elon berish/Sotish</span>
+            </button>
+            <button className="btn">
+              <span>Sotib olish</span>
+            </button>
+          </div>
+          <div className="statistics">
+            <div className="content">
+              <h3>100+</h3>
+              <p>Pubg accountlar</p>
+            </div>
+            <div className="content project">
+              <h3>100+</h3>
+              <p>Ishonchli sotuvchilar</p>
+            </div>
+            <div className="content">
+              <h3>500+</h3>
+              <p>Sotilgan accountlar</p>
+            </div>
+          </div>
         </div>
 
-        <div className='statistics'>
-          <div className='content'>
-            <h3>100+</h3>
-            <p>Pubg accountlar</p>
-          </div>
-          <div className='content project'>
-            <h3>100+</h3>
-            <p>Ishonchli sotuvchilar</p>
-          </div>
-          <div className='content'>
-            <h3>500+</h3>
-            <p>Sotilgan accountlar</p>
+        <div className="home-container__img">
+          <div className="home-img">
+            <img src={homeImg} alt="Game" />
           </div>
         </div>
+        
+        <div className="statistics">
+            <div className="content">
+              <h3>100+</h3>
+              <p>Pubg accountlar</p>
+            </div>
+            <div className="content project">
+              <h3>100+</h3>
+              <p>Ishonchli sotuvchilar</p>
+            </div>
+            <div className="content">
+              <h3>500+</h3>
+              <p>Sotilgan accountlar</p>
+            </div>
+          </div>
       </div>
     </section>
   );
