c660d89efb2fe3009443b69d7847462373e5aca2
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-01-10 19:57:19 +0500

    Add first login designs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './component/header/Header';
 import Footer from './component/footer/Footer';
@@ -7,17 +7,19 @@ import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Basket from './pages/basket/Basket';
 import Contact from './pages/contact/Contact';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <Header />
 
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/basket" element={<Basket />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path='/' exact element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/basket' element={<Basket />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' exact element={<Login />} />
       </Routes>
 
       <Footer />
