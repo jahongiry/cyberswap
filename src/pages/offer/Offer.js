f865a7bd3ccfbd31462ea35ffccc8ab8315c03db
azimjonking <azimjonjalilov1301@mail.ru>
2024-01-11 16:49:22 +0200

    update

---
+++ src/pages/offer/Offer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./offer.css";
+
+const Offer = () => {
+  return (
+    <section className='offer container'>
+      <h1>Offer</h1>
+    </section>
+  )
+}
+
+export default Offer
