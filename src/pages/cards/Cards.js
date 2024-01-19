30c7266d46ff5e113e168e6f68de0464fcb80c35
azimjonking <azimjonjalilov1301@mail.ru>
2024-01-19 20:03:37 +0200

    fix card media

--- src/pages/cards/Cards.js
+++ src/pages/cards/Cards.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import './cards.css';
-import logo1 from '../../component/header/logo1_1.png';
-import pubg from '../../img/pubg.jpeg';
-import { NavLink, Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-import Popup from './popup/Popup';
+import React, { useState } from "react";
+import "./cards.css";
+import logo1 from "../../component/header/logo1_1.png";
+import pubg from "../../img/pubg.jpeg";
+import { NavLink, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import Popup from "./popup/Popup";
 
 const Cards = () => {
   const [selectedGameId, setSelectedGameId] = useState(null);
@@ -18,46 +18,46 @@ const Cards = () => {
     {
       id: 1,
       img: pubg,
-      name: 'game1',
+      name: "game1",
       level: 62,
-      rp: '3 ta olingan',
+      rp: "3 ta olingan",
       skins: "Juda ko'p",
       price: 2000,
-      owner: 'Jahongir',
-      description: 'assdfsdf sadfsdfsfd sdfsdfsdfsf sdfsdfsdfsdfs  sdfsfsfsd',
+      owner: "Jahongir",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, ipsa!",
     },
     {
       id: 2,
       img: pubg,
-      name: 'game1',
+      name: "game1",
       level: 62,
-      rp: '3 ta olingan',
+      rp: "3 ta olingan",
       skins: "Juda ko'p",
       price: 2000,
-      owner: 'Jahongir',
-      description: 'assdfsdf sadfsdfsfd sdfsdfsdfsf sdfsdfsdfsdfs  sdfsfsfsd',
+      owner: "Jahongir",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, ipsa!",
     },
     {
       id: 3,
       img: pubg,
-      name: 'game1',
+      name: "game1",
       level: 62,
-      rp: '3 ta olingan',
+      rp: "3 ta olingan",
       skins: "Juda ko'p",
       price: 2000,
-      owner: 'Jahongir',
-      description: 'assdfsdf sadfsdfsfd sdfsdfsdfsf sdfsdfsdfsdfs  sdfsfsfsd',
+      owner: "Jahongir",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, ipsa!",
     },
     {
       id: 4,
       img: pubg,
-      name: 'game1',
+      name: "game1",
       level: 62,
-      rp: '3 ta olingan',
+      rp: "3 ta olingan",
       skins: "Juda ko'p",
       price: 2000,
-      owner: 'Jahongir',
-      description: 'assdfsdf sadfsdfsfd sdfsdfsdfsf sdfsdfsdfsdfs  sdfsfsfsd',
+      owner: "Jahongir",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, ipsa!",
     },
   ];
 
@@ -67,46 +67,56 @@ const Cards = () => {
   const selectedGame = games.find((game) => game.id === selectedGameId);
 
   return (
-    <div>
-      <div className='cards-container'>
-        <div className='search-container'>
+    <div className="cards-wrapper">
+      <div className="cards-container">
+        <div className="search-container">
           <input
-            type='text'
+            type="text"
             placeholder="Kerakli so'zini yozing"
-            className='search-input'
+            className="search-input"
           />
-          <button className='search-button'>Qidirish</button>
+          <button className="search-button">Qidirish</button>
         </div>
-        <h4 className='filterlash'>Filterlash</h4>
-        <div className='filter-container'>
-          <button className='filter-button active'>Mashxurlar</button>
-          <button className='filter-button'>Yangilari</button>
-          <button className='filter-button'>Arzonlari</button>
-          <button className='filter-button'>Qimmatlari</button>
+        <h4 className="filterlash">Filterlash</h4>
+        <div className="filter-container">
+          <button className="filter-button active">Mashxurlar</button>
+          <button className="filter-button">Yangilari</button>
+          <button className="filter-button">Arzonlari</button>
+          <button className="filter-button">Qimmatlari</button>
         </div>
       </div>
-      <div className='cards-section'>
+      <div className="cards-section">
         {games.map((game) => (
-          <div key={game.id} className='card'>
-            <img
-              onClick={() => openPopUp(game.id)}
-              className='card-img'
-              src={game.img}
-              alt='image'
-            />
-            <h5 onClick={() => openPopUp(game.id)}>{game.name}</h5>
-            <p onClick={() => openPopUp(game.id)}>Level: {game.level}</p>
-            <p onClick={() => openPopUp(game.id)}>RP: {game.rp}</p>
-            <p onClick={() => openPopUp(game.id)}>Skins: {game.skins}</p>
-            <p onClick={() => openPopUp(game.id)}>Owner: {game.owner}</p>
-            <div className='stars' onClick={() => openPopUp(game.id)}>
-              <FontAwesomeIcon icon={faStar} className='icon-gold' />
-              <FontAwesomeIcon icon={faStar} className='icon-gold' />
-              <FontAwesomeIcon icon={faStar} className='icon-gold' />
-              <FontAwesomeIcon icon={faStar} className='icon-gold' />
-              <FontAwesomeIcon icon={faStar} className='icon-gold' />
+          <div key={game.id} className="card">
+            <div className="card-img">
+              <img
+                onClick={() => openPopUp(game.id)}
+                className="card-img"
+                src={game.img}
+                alt="image"
+              />
             </div>
-            <button onClick={() => openPopUp(game.id)} className='price-button'>
+            <h5 onClick={() => openPopUp(game.id)}>{game.name}</h5>
+            <p onClick={() => openPopUp(game.id)}>
+              <span>Level:</span> {game.level}
+            </p>
+            <p onClick={() => openPopUp(game.id)}>
+              <span>RP:</span> {game.rp}
+            </p>
+            <p onClick={() => openPopUp(game.id)}>
+              <span>Skins:</span> {game.skins}
+            </p>
+            <p onClick={() => openPopUp(game.id)}>
+              <span>Owner:</span> {game.owner}
+            </p>
+            <div className="stars" onClick={() => openPopUp(game.id)}>
+              <ion-icon name="star" className="icon-gold"></ion-icon>
+              <ion-icon name="star" className="icon-gold"></ion-icon>
+              <ion-icon name="star" className="icon-gold"></ion-icon>
+              <ion-icon name="star" className="icon-gold"></ion-icon>
+              <ion-icon name="star-half" className="icon-gold"></ion-icon>
+            </div>
+            <button onClick={() => openPopUp(game.id)} className="price-button">
               {game.price} UZS
             </button>
             {selectedGameId && (
