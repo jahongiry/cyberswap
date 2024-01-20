75789ed1cbc6fe29878dbf57c94f193943bc0e6c
azimjonking <azimjonjalilov1301@mail.ru>
2024-01-20 19:01:09 +0200

    fix hamburger and scroll

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import halmet from '../../img/halmet.png';
+
 import './header.css';
+
 import logo from './logo1_1.png';
-import { useSelector } from 'react-redux';
-import { selectCurrentUser } from '../../slices/authSlice';
 
 const Header = () => {
   const [navbar, setNavbar] = useState(false);
-  const user = useSelector(selectCurrentUser);
-
   const navBtn = () => setNavbar(!navbar);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        setNavbar(false)
+      }
+    });
+  }, []);
+
+
   return (
     <header className='container header'>
       <Link to='/'>
@@ -20,38 +26,29 @@ const Header = () => {
         <span>Cyberswap</span>
       </Link>
 
-      <nav className={navbar ? 'mobile' : ''}>
+      <nav className={navbar ? 'mobile' : ''} onClick={navBtn}>
         <ul>
           <li onClick={navBtn}>
             <NavLink activeclassname='active' to='/'>
-              <i className='fa-solid fa-house'></i>
+            <i className="fa-solid fa-house"></i>
               Asosiy
             </NavLink>
           </li>
           <li onClick={navBtn}>
-            <NavLink to='/advertisement'>
-              <i className='fa-solid fa-circle-info'></i>E'lonlar
-            </NavLink>
+            <NavLink to='/cards'><i className="fa-solid fa-circle-info"></i>E'lonlar</NavLink>
           </li>
           <li onClick={navBtn}>
-            <NavLink to='/contact'>
-              <i className='fa-solid fa-comments'></i>Bog'lanish
-            </NavLink>
+            <NavLink to='/contact'><i className="fa-solid fa-comments"></i>Bog'lanish</NavLink>
           </li>
         </ul>
       </nav>
 
       <div className='header-btns'>
-        {user ? (
-          <Link to='/profile' className='sign-in'>
-            <img src={halmet} className='halmet'></img>
-          </Link>
-        ) : (
-          <Link to='/login' className='sign-in'>
-            <span>Kirish</span>
-            <i className='fa fa-sign-in' aria-hidden='true'></i>
-          </Link>
-        )}
+        <Link to='/login' className='sign-in'>
+          <span>Kirish</span>
+          <i className='fa fa-sign-in' aria-hidden='true'></i>
+        </Link>
+
         <Link className='nav-btn' onClick={navBtn}>
           <i className={navbar ? 'fa-solid fa-times' : 'fa-solid fa-bars'}></i>
         </Link>
