8a19e17a377bc3e37582ee9ab553e08184f6192c
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-02-19 19:38:09 +0500

    Add Safary

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -53,10 +53,19 @@ const Chat = () => {
   };
 
   const getMilitaryTime = (dateString) => {
-    const date = new Date(dateString);
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    return `${hours}:${minutes}`;
+    if (!dateString || typeof dateString !== 'string') {
+      return 'Invalid Date';
+    }
+
+    try {
+      const date = new Date(dateString);
+      const hours = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
+
+      return `${hours}:${minutes}`;
+    } catch (e) {
+      console.log({ dateString });
+    }
   };
 
   useEffect(() => {
